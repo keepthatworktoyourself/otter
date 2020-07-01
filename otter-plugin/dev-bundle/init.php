97363421691b78e3ba5ce79fefe144c3eb84c972
Ben Hallstein <a@ben.am>
2020-07-01 21:20:14 +0100

    otter-plugin/dev-bundle: blockset.js -> blocks.js

--- otter-plugin/dev-bundle/init.php
+++ otter-plugin/dev-bundle/init.php
@@ -1,6 +1,6 @@
 <?php
   add_action('init', function() {
-    \Otter\otter_for_post_type('post', '../dev-bundle/dist/blockset.js');
-    \Otter\css_bundle('../dev-bundle/dist/blockset.css');
+    \Otter\otter_for_post_type('post', '../dev-bundle/dist/blocks.js');
+    \Otter\css_bundle('../dev-bundle/dist/blocks.css');
   });
 
