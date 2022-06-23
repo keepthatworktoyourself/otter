34f2c62cb1cece5a68ec80447ddcf6555f3e7fcb
pjrundle <petejrundle@gmail.com>
2022-06-23 13:31:20 +0100

    minor fix to block picker layout

--- src/core/components/otter/other/BlockPicker.jsx
+++ src/core/components/otter/other/BlockPicker.jsx
@@ -18,57 +18,59 @@ export default function BlockPicker({
     <div className="relative p-10 bg-white"
          style={{width: '95vw', maxWidth: '1200px'}}
     >
-      {block_group_keys.map((k, i) => (
-        <div key={i}
-             className={classNames(
-               i !== 0 && 'border-t pt-7',
-               i === 0 && 'pb-7',
-               theme_ctx.classes.skin.border_color,
-             )}
-        >
-          <h3 className={classNames(
-            'text-sm mb-4',
-            theme_ctx.classes.typography.heading,
-          )}
+      {block_group_keys.map((k, i) => {
+        const isLast = block_group_keys.length - 1 === i
+
+        return (
+          <div key={i}
+               className={classNames(
+                 i !== 0 && 'border-t pt-9',
+                 !isLast && 'pb-9',
+                 theme_ctx.classes.skin.border_color,
+               )}
           >
-            {blocks[k].name || k}
-          </h3>
+            <h3 className={classNames(
+              'text-sm mb-4 leading-none',
+              theme_ctx.classes.typography.heading,
+            )}
+            >
+              {blocks[k].name || k}
+            </h3>
 
-          <div className="grid grid-cols-4 gap-10">
-            {blocks[k].blocks.map(block => (
-              <figure className={classNames(
-                'cursor-pointer',
-              )}
-                      key={block.type}
-                      onClick={ev => {
-                        set_block_to_insert({
-                          type:  ev.currentTarget.getAttribute('data-block-type'),
-                          index: block_index,
-                        })
-                        close?.()
-                      }}
-                      data-block-type={block.type}
-              >
-                <div className={classNames(
-                  'bg-center bg-cover aspect-16x10 border',
-                  theme_ctx.classes.skin.border_color,
-                )}
-                     style={{
-                       backgroundImage: 'url(' + (block.thumbnail || 'https://res.cloudinary.com/drtjqpz13/image/upload/v1636189866/Wombat/hero-b.png') + ')',
-                     }} />
-
-                <figcaption className={classNames(
-                  'text-xs pt-3',
-                  theme_ctx.classes.typography.heading,
-                )}
+            <div className="grid grid-cols-4 gap-10">
+              {blocks[k].blocks.map(block => (
+                <figure className="cursor-pointer"
+                        key={block.type}
+                        onClick={ev => {
+                          set_block_to_insert({
+                            type:  ev.currentTarget.getAttribute('data-block-type'),
+                            index: block_index,
+                          })
+                          close?.()
+                        }}
+                        data-block-type={block.type}
                 >
-                  {block.description || humanify_str(block.type)}
-                </figcaption>
-              </figure>
-            ))}
+                  <div className={classNames(
+                    'bg-center bg-cover aspect-16x10 border',
+                    theme_ctx.classes.skin.border_color,
+                  )}
+                       style={{
+                         backgroundImage: 'url(' + (block.thumbnail || 'https://res.cloudinary.com/drtjqpz13/image/upload/v1636189866/Wombat/hero-b.png') + ')',
+                       }} />
+
+                  <figcaption className={classNames(
+                    'text-xs pt-2',
+                    theme_ctx.classes.typography.sub_heading,
+                  )}
+                  >
+                    {block.description || humanify_str(block.type)}
+                  </figcaption>
+                </figure>
+              ))}
+            </div>
           </div>
-        </div>
-      ))}
+        )
+      })}
     </div>
   )
 }
